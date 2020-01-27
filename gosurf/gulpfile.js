afcb5363bc562396fe861eaedbfa57970e887d92
IgorMuramshchikov <igor.muram@gmail.com>
2020-01-27 09:56:09 +0300

    Create gulpfile.js

---
+++ gosurf/gulpfile.js
@@ -0,0 +1,99 @@
+var	syntax        = 'sass'; /* Syntax: sass or scss; */
+   	gmWatch        = false; /* ON/OFF GraphicsMagick watching "img/_src" folder (true/false). Linux install gm: sudo apt update; sudo apt install graphicsmagick */
+
+var	gulp          = require('gulp'),
+   	gutil         = require('gulp-util' ),
+   	sass          = require('gulp-sass'),
+   	browserSync   = require('browser-sync'),
+   	concat        = require('gulp-concat'),
+   	terser        = require('gulp-terser'),
+   	cleancss      = require('gulp-clean-css'),
+   	rename        = require('gulp-rename'),
+   	autoprefixer  = require('gulp-autoprefixer'),
+   	notify        = require('gulp-notify'),
+   	imageResize   = require('gulp-image-resize'),
+   	imagemin      = require('gulp-imagemin'),
+   	del           = require('del');
+
+/* Local Server */
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+	})
+});
+
+/* Sass|Scss Styles */
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss({ level: { 1: { specialComments: 0 } } })) /* Opt., comment out when debugging */
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+/* JS */
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-carousel/slick/slick.js',
+		'app/libs/wow/dist/wow.min.js',
+		'app/js/common.js',                             /* Always at the end */
+	])
+	.pipe(concat('scripts.min.js'))
+	.pipe(terser())                               		/* Minify js (opt.) */
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+/* HTML Live Reload */
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+/* Clean @*x IMG's */
+gulp.task('cleanimg', function() {
+	return del(['app/img/@*'], { force: true })
+});
+
+/* Remove folder 'dist' */
+gulp.task('removedist', function(done) { 
+	return del(['dist']); 
+	done();
+});
+
+/* Build */
+gulp.task('build', gulp.series('removedist', 'styles', 'scripts', function(ContinueProcess) {
+
+	ContinueProcess();
+
+	var buildFiles = gulp.src([
+		'app/*.html',
+		'app/.htaccess',
+		]).pipe(gulp.dest('dist'));
+
+	var buildCss = gulp.src([
+		'app/css/main.min.css',
+		]).pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src([
+		'app/js/scripts.min.js',
+		]).pipe(gulp.dest('dist/js'));
+
+	var buildImg = gulp.src([
+		'app/img/*',
+		]).pipe(gulp.dest('dist/img'));
+
+}));
+
+gulp.task('watch', function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+});
+gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));

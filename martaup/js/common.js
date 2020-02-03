b7b6761a33a6d5fd27f33a8b64fdc061d5dd0d5d
IgorMuramshchikov <igor.muram@gmail.com>
2020-02-03 17:36:44 +0300

    Update js

--- martaup/js/common.js
+++ martaup/js/common.js
@@ -1,6 +1,10 @@
 $(function() {
+
+	/* Preloader */
 	$('.preloader').delay(1500).fadeOut('slow');
 	
+
+	/* Slider */
 	$('.header-slider').slick({
 		infinite: true,
 		autoplay: true,
@@ -8,13 +12,11 @@ $(function() {
 		vertical: true,
 		dots: true,
 		dotsClass: 'header-dots',
-		arrows: false,
-		responsive: [{
-			breakpoint: 900,
-			settings: "unslick"
-		}]
+		arrows: false
 	});
 
+
+	/* Back to top button */
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 50)
 			$('.top').addClass('active');
@@ -26,6 +28,8 @@ $(function() {
 		$('html,body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 	});
 
+
+	/* Menu */
 	$("#navToggle").on("click", function(e) {
 		e.preventDefault();
 		$(".menu__list").slideToggle();

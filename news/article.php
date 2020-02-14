82b45f76acfd246f3a10545099f4e5c6343e9e27
IgorMuramshchikov <igor.muram@gmail.com>
2020-02-14 05:03:39 +0300

    Update index.php and article.php

--- news/article.php
+++ news/article.php
@@ -2,24 +2,25 @@
 <html lang="ru">
 
 <?php
-$title="Статья";
-require("head.php");
+$title = "Статья";
+require("php/blocks/head.php");
 ?>
 
 <body>
 	<?php
-	require("preloader.php");
+	require("php/blocks/preloader.php");
 	?>
 
 	<div class="container">
 		<?php
-		require("header.php");
-		require("article-main.php");
-		require("footer.php");
+		require("php/blocks/header.php");
+		require("php/article/article-main.php");
+		require("php/blocks/footer.php");
 		?>
 	</div>
-
+	
 
 	<script src="js/scripts.min.js"></script>
 </body>
+
 </html>
--- news/index.php
+++ news/index.php
@@ -1,30 +1,26 @@
-<?php
-session_start();
-//require("functions.php"); 
-//connectDB();
-?>
-
 <!DOCTYPE html>
 <html lang="ru">
 
 <?php
-require("head.php");
+$title="Новости";
+require("php/blocks/head.php");
 ?>
 
 <body>
 	<?php
-	require("preloader.php");
+	require("php/blocks/preloader.php");
 	?>
 
 	<div class="container">
 		<?php
-		require("header.php");
-		require("index-main.php");
-		require("footer.php");
+		require("php/blocks/header.php");
+		require("php/index/index-main.php");
+		require("php/blocks/footer.php");
 		?>
 	</div>
 
 
 	<script src="js/scripts.min.js"></script>
 </body>
+
 </html>

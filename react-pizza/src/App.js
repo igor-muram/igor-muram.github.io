7f8ea8a5fd6ba9db3843c35e1c566a8119e4608f
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-22 19:26:14 +0700

    Add PizzaBlock component + loading pizzas + fix grid bug

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,17 +1,25 @@
+import React from 'react';
+
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((r) => r.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
       </div>
     </div>
   );
 }
-
-export default App;

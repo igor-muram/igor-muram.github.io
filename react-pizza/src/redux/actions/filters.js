3d3aa33645d87a2290ad63497500e7773ab9fda8
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-29 01:03:23 +0700

    Fix filters

--- react-pizza/src/redux/actions/filters.js
+++ react-pizza/src/redux/actions/filters.js
@@ -1,9 +1,9 @@
-const setSortBy = (name) => ({
+export const setSortBy = (name) => ({
   type: 'SET_SORT_BY',
   payload: name,
 });
 
-const setCategory = (categoryIndex) => ({
+export const setCategory = (categoryIndex) => ({
   type: 'SET_CATEGORY',
   payload: categoryIndex,
 });
--- react-pizza/src/redux/reducers/filters.js
+++ react-pizza/src/redux/reducers/filters.js
@@ -1,5 +1,5 @@
 const initialState = {
-  category: 0,
+  category: null,
   sortBy: 'popular',
 };
 
@@ -8,6 +8,12 @@ const filters = (state = initialState, action) => {
     return {
       ...state,
       sortBy: action.payload,
+    };
+  }
+  if (action.type === 'SET_CATEGORY') {
+    return {
+      ...state,
+      category: action.payload,
     };
   }
   return state;

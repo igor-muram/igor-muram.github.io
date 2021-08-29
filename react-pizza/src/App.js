5aa0863539d9edc202a51a7927794b70a9157b0d
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-29 15:37:11 +0700

    Add async action using redux-thunk

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 export default function App() {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    dispatch(fetchPizzas());
   }, []);
 
   return (
--- react-pizza/src/redux/actions/pizzas.js
+++ react-pizza/src/redux/actions/pizzas.js
@@ -1,3 +1,11 @@
+import axios from 'axios';
+
+export const fetchPizzas = () => (dispatch) => {
+  axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+    dispatch(setPizzas(data));
+  });
+};
+
 export const setPizzas = (items) => ({
   type: 'SET_PIZZAS',
   payload: items,

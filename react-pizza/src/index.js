09769340fc4bdc5892a83359a84cf954f5922d42
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-30 06:55:02 +0700

    Remove reportWebVitals

--- react-pizza/src/index.js
+++ react-pizza/src/index.js
@@ -7,7 +7,6 @@ import store from './redux/store';
 import './scss/app.scss';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Router>
@@ -17,8 +16,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

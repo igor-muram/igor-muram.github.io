ec39160bce8095e4ed4baf4920571c18a00a16e8
IgorMuramshchikov <igor.muram@gmail.com>
2021-08-30 08:19:30 +0700

    Add adaptivity

--- react-pizza/src/components/SortPopup.jsx
+++ react-pizza/src/components/SortPopup.jsx
@@ -35,20 +35,24 @@ const SortPopup = React.memo(function SortPopup({
   return (
     <div className="sort" ref={sortRef}>
       <div className="sort__label">
-        <svg
-          className={visiblePopup ? 'rotated' : ''}
-          width="10"
-          height="6"
-          viewBox="0 0 10 6"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-            fill="#2C2C2C"
-          />
-        </svg>
-        <b>Сортировка по:</b>
-        <span onClick={toggleVisiblePopup}>{activeLabel}</span>
+        <span>
+          <svg
+            className={visiblePopup ? 'rotated' : ''}
+            width="10"
+            height="6"
+            viewBox="0 0 10 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+              fill="#2C2C2C"
+            />
+          </svg>
+          <b>Сортировка по:</b>
+        </span>
+        <span className="sort__label-text" onClick={toggleVisiblePopup}>
+          {activeLabel}
+        </span>
       </div>
       {visiblePopup && (
         <div className="sort__popup">
--- react-pizza/src/pages/Cart.jsx
+++ react-pizza/src/pages/Cart.jsx
@@ -121,10 +121,10 @@ export default function Cart() {
           <div className="cart__bottom">
             <div className="cart__bottom-details">
               <span>
-                Всего пицц: <b>{totalCount} шт.</b>
+                Всего пицц:&nbsp;<b>{totalCount} шт.</b>
               </span>
               <span>
-                Сумма заказа: <b>{totalPrice} ₽</b>
+                Сумма заказа:&nbsp;<b>{totalPrice} ₽</b>
               </span>
             </div>
             <div className="cart__bottom-buttons">

e36c3adba9f716ccb488fdc3d5a6f4f5773a3dfe
IgorMuramshchikov <igor.muram@gmail.com>
2021-09-19 20:01:36 +0700

    Add simple default animations for post-list

--- vue-ui/src/components/PostList.vue
+++ vue-ui/src/components/PostList.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="post-list" v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h3 v-else style="color: red">Список постов пуст</h3>
 </template>
@@ -23,4 +30,14 @@ export default {
 <style lang="sass" scoped>
 .post-list
 	margin-top: 15px
+	&-item
+		display: inline-block
+		margin-right: 10px
+	&-enter-active, &-leave-active
+		transition: all .4s ease
+	&-enter-from, &-leave-to
+		opacity: 0
+		transform: translateX(130px)
+	&-move
+		transition: transform .4s ease
 </style>

6ffd6d4d91e8ade43caf5dcb53eb2a26667613f6
IgorMuramshchikov <igor.muram@gmail.com>
2021-09-19 19:48:12 +0700

    Create Select component

---
+++ vue-ui/src/components/UI/UISelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <select v-model="modelValue" @change="changeOption">
+    <option disabled value="">Выберите значение</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'ui-select',
+  props: {
+    modelValue: {
+      type: String,
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    changeOption(event) {
+      this.$emit('update:modelValue', event.target.value);
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue-ui/src/components/UI/index.js
+++ vue-ui/src/components/UI/index.js
@@ -1,5 +1,6 @@
 import UIButton from '@/components/UI/UIButton';
 import UIInput from '@/components/UI/UIInput';
 import UIDialog from '@/components/UI/UIDialog';
+import UISelect from '@/components/UI/UISelect';
 
-export default [UIButton, UIInput, UIDialog];
+export default [UIButton, UIInput, UIDialog, UISelect];
